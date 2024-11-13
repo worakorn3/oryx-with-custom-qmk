bcf0bae963cd4c0acd6b5c03f05947e95bb96c2d
worakorn3 <21075895+worakorn3@users.noreply.github.com>
2024-11-13 20:32:13 +0700

    Update achordion.c

--- XeEZ9/features/achordion.c
+++ XeEZ9/features/achordion.c
@@ -340,7 +340,15 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
 
 // By default, the timeout is 1000 ms for all keys.
 __attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
-  return 1000;
+  switch (tap_hold_keycode) {
+    case KC_ENTER:
+    case KC_BACKSPACE:
+    case KC_V:
+      return 0;  // Bypass Achordion for these keys.
+  }
+
+  // customized 800 ms
+  return 800;
 }
 
 // By default, Shift and Ctrl mods are eager, and Alt and GUI are not.
